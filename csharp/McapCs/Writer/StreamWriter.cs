4c40f2eb5810e76e7b33a9ee821802acb3f7ccd3
ssaattww <taiga202500@gmail.com>
2025-07-13 14:45:52 +0900

    Add StreamWriter class implementing IDisposable and writable stream functionality

---
+++ csharp/McapCs/Writer/StreamWriter.cs
@@ -0,0 +1,63 @@
+using System.IO;
+
+namespace McapCs.Writer;
+
+public class StreamWriter : Writable, IDisposable
+{
+  private readonly Stream? _stream;
+  private ulong _size = 0;
+
+  public StreamWriter(Stream stream)
+  {
+    _stream = stream;
+    _size = 0;
+  }
+
+  protected override void HandleWrite(byte[] data)
+  {
+    if (data == null || data.Length == 0)
+    {
+      return;
+    }
+
+    var size = (ulong)data.Length;
+    if (_stream == null)
+    {
+      throw new InvalidOperationException("StreamWriter is not initialized with a stream.");
+    }
+
+    _stream.Write(data, 0, (int)size);
+    _size += size;
+
+
+  }
+  public void Dispose()
+  {
+    End();
+  }
+  public override void Flush()
+  {
+    if (_stream == null)
+    {
+      throw new InvalidOperationException("StreamWriter is not initialized with a stream.");
+    }
+    _stream.Flush();
+  }
+
+  public override void End()
+  {
+    if (_stream == null)
+    {
+      throw new InvalidOperationException("StreamWriter is not initialized with a stream.");
+    }
+    Flush();
+    _stream?.Close();
+    _size = 0;
+    _stream?.Dispose();
+  }
+
+  public override ulong Size
+  {
+    get { return _size; }
+  }
+}
